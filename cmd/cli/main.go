04d7792e1337eae5ce49e40d79daf2aba2622fcc
Bartal Læarsson <bl@sev.fo>
2025-03-05 10:23:44 +0000

    remove old files

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "github.com/bxtal-lsn/go-boilme/cmd/cli/cmd"
+import (
+	"os"
 
-func main() {
-	cmd.Execute()
+	"github.com/bxtal-lsn/go-boilme"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+const version = "1.0.0"
+
+var boil boilme.Boilme
+
+// rootCmd represents the base command
+var rootCmd = &cobra.Command{
+	Use:   "boilme",
+	Short: "A powerful web application framework for Go",
+	Long: `Boilme is a powerful web application framework for Go.
+It provides tools for database migrations, authentication, and more.`,
 }
 
+func main() {
+	// Initialize cobra commands
+	initCommands()
+
+	// Execute the root command
+	if err := rootCmd.Execute(); err != nil {
+		exitGracefully(err)
+	}
+}
+
+func exitGracefully(err error, msg ...string) {
+	message := ""
+	if len(msg) > 0 {
+		message = msg[0]
+	}
+
+	if err != nil {
+		color.Red("Error: %v\n", err)
+	}
+
+	if len(message) > 0 {
+		color.Yellow(message)
+	} else {
+		color.Green("Finished!")
+	}
+
+	os.Exit(0)
+}
+
+// initCommands adds all child commands to the root command
+func initCommands() {
+	// Add version command
+	rootCmd.AddCommand(versionCmd)
+}
--- cmd/cli/version.go
+++ cmd/cli/version.go
@@ -1,22 +1,15 @@
-package cmd
+package main
 
 import (
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
-const version = "1.0.0"
-
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
-	Long:  `Print the current version of Boilme.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		color.Yellow("Boilme Framework version: %s", version)
+		color.Yellow("Application version: " + version)
 	},
-}
-
-func init() {
-	rootCmd.AddCommand(versionCmd)
 }
